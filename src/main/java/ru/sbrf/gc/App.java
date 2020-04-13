c0f049c7381beb6d0e0af04eeaac2d634db3bc3e
kyumo <akubarev@gmail.com>
2020-04-13 21:35:34 +0300

    первый коммит

---
+++ src/main/java/ru/sbrf/gc/App.java
@@ -0,0 +1,13 @@
+package ru.sbrf.gc;
+
+/**
+ * Hello world!
+ *
+ */
+public class GCTester
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
